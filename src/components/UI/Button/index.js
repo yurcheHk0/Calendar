a8ae24d405081c4743a8a28f0ebc8b02d5dd791a
Yuriy <yurcheHk0@gmail.com>
2021-11-20 22:39:10 +0200

    - react-scripts updated to the latest version - Button component code refactoring

--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import styles from "./button.module.scss";
 
-export const Button = ({txt, color, handleClick}) =>
-    <button className={`${styles.btn} ${color ? styles[color] : styles.default}`} onClick={handleClick}>{txt}</button>;
-
-export default Button;
+export default
+function Button(
+    {
+        txt,
+        color,
+        handleClick
+    }
+) {
+   return <button
+        className={`${styles.btn} ${color ? styles[color] : styles.default}`}
+        onClick={handleClick}
+    >
+        {txt}
+    </button>
+};
